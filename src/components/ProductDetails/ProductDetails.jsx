cfd7e8537d13f34b5a00789d94942c186e9e04a0
salauddin kamran <kamransalauddin186@gmail.com>
2026-03-04 23:07:41 +0600

    this is five commit in the project

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -4,7 +4,19 @@ import { useLoaderData } from "react-router";
 const ProductDetails = () => {
   const product = useLoaderData();
   console.log(product);
-  return <div>products details</div>;
+  return (
+    <div>
+      <div className="flex items-center">
+        <div className="w-full">
+          <img src={product.image} alt="" />
+        </div>
+        <div className="w-full">
+          <h3 className="text-3xl font-bold mb-3">{product.title}</h3>
+          <button className="btn btn-primary">I want to By this product</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
