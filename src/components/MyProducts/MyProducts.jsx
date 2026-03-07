51a746aebe426d17654527fa839c8d4e479643d0
salauddin kamran <kamransalauddin186@gmail.com>
2026-03-07 23:09:28 +0600

    this is six commit in the project

--- src/components/MyProducts/MyProducts.jsx
+++ src/components/MyProducts/MyProducts.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MyProducts = () => {
   return (
     <div>
-      this product page
+       This is my product page
     </div>
   );
 };
--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, { use, useRef } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 import { useLoaderData } from "react-router";
 import { AuthContext } from "../../Contexts/AuthContext";
 import { toast } from "react-toastify";
@@ -13,6 +13,7 @@ const ProductDetails = () => {
   } = useLoaderData();
   const bidsModalRef = useRef(null);
   const { user } = use(AuthContext);
+  const [bids, setBids] = useState([]);
 
   const handleBidAmount = (e) => {
     e.preventDefault();
@@ -20,7 +21,7 @@ const ProductDetails = () => {
     const email = e.target.email.value;
     const bid = e.target.bid.value;
     console.log(productId, name, email, bid);
-    const newBids = {
+    const newBid = {
       product: productId,
       buyer_name: name,
       buyer_email: email,
@@ -32,22 +33,42 @@ const ProductDetails = () => {
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify(newBids),
+      body: JSON.stringify(newBid),
     })
       .then((res) => res.json())
       .then((data) => {
         console.log("After placeing bid", data);
-        toast("Your bid place successfuly!")
+        toast("Your bid place successfuly!");
+        if (data.insertedId) {
+          bidsModalRef.current.close();
+          newBid._id = data.insertedId;
+          const newBids = [...bids, newBid];
+          newBids.sort((a, b) => b.buyer_price - a.buyer_price);
+          setBids(newBids);
+        }
       })
       .catch((error) => {
         console.log(error);
       });
   };
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/products/bids/${productId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("bids fot this product", data);
+        setBids(data);
+      })
+      .catch((error) => {
+        ``;
+        console.log(error);
+      });
+  }, [productId]);
   return (
     <div>
       <div className="flex items-center gap-5">
         <div className="w-full">
-          <img src={image} alt="" />
+          <img className="" src={image} alt="" />
         </div>
         <div className="w-full">
           <h3 className="text-3xl font-bold mb-3">{title}</h3>
@@ -106,6 +127,55 @@ const ProductDetails = () => {
               </div>
             </div>
           </dialog>
+        </div>
+      </div>
+      <div className="mt-10">
+        <h3 className="text-3xl font-bold">
+          Bids fot this Product:{" "}
+          <span className="text-primary">{bids.length}</span>
+        </h3>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>SL NO.</th>
+                <th>Buyer Name</th>
+                <th>Buyer Email</th>
+                <th>Bid Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {bids.map((bid, index) => (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{bid.buyer_name}</div>
+                        <div className="text-sm opacity-50">United States</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{bid.buyer_email}</td>
+                  <td>{bid.buyer_price}</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
